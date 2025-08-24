d5077adc4d63fa416d989e723c673afb4a27c8d2
marcolanfranchi <marcolanfranchi02@gmail.com>
2025-08-24 07:15:38 -0700

    Changed auth to use spotify refresh token

--- src/auth.py
+++ src/auth.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from pathlib import Path
 
 load_dotenv()
 
@@ -12,23 +11,29 @@ SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI", "http://localhost:8888/callback")
 SCOPE = "playlist-read-private user-library-read user-read-recently-played"
 
-cache_path = str(Path.home() / ".spotify_token")
+CACHE_PATH = f"./.spotify_token"
 
 sp_oauth = SpotifyOAuth(
     client_id=SPOTIFY_CLIENT_ID,
     client_secret=SPOTIFY_CLIENT_SECRET,
     redirect_uri=SPOTIFY_REDIRECT_URI,
     scope=SCOPE,
-    cache_path=cache_path
+    cache_path=CACHE_PATH
 )
 
 def get_spotify_client():
     token_info = sp_oauth.get_cached_token()
     if not token_info:
+        # first-time auth (need to do manually)
         auth_url = sp_oauth.get_authorize_url()
         print(f"Visit this URL to authorize: {auth_url}")
         response = input("Paste the URL you were redirected to: ")
         code = sp_oauth.parse_response_code(response)
-        token_info = sp_oauth.get_access_token(code)
+        token_info = sp_oauth.get_access_token(code, as_dict=True)
+
+    # always refresh if needed
+    if sp_oauth.is_token_expired(token_info):
+        token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
+
     return spotipy.Spotify(auth=token_info["access_token"])
 
